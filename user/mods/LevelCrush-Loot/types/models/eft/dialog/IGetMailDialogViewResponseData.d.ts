4448d1c1da1c1a0b031aebb43720b1b633e36c79
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-04-28 10:31:12 -0400

    Auto buld dependencies

---
+++ user/mods/LevelCrush-Loot/types/models/eft/dialog/IGetMailDialogViewResponseData.d.ts
@@ -0,0 +1,9 @@
+import {
+  IUserDialogInfo,
+  Message,
+} from "@spt-aki/models/eft/profile/IAkiProfile";
+export interface IGetMailDialogViewResponseData {
+  messages: Message[];
+  profiles: IUserDialogInfo[];
+  hasMessagesWithRewards: boolean;
+}
