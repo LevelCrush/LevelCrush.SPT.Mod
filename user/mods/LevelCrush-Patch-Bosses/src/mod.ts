866d06653be6700937b3ad22548f43fee2e9ba00
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-05-04 12:48:57 -0400

    Boss Patcher can now delete unneeded properties

--- user/mods/LevelCrush-Patch-Bosses/src/mod.ts
+++ user/mods/LevelCrush-Patch-Bosses/src/mod.ts
@@ -134,17 +134,24 @@ class LC_Patch_Bosses implements IPreAkiLoadMod, IPostDBLoadMod {
       }
       const is_array = Array.isArray(source[prop]);
       const current_v = source[prop];
+      const new_v_is_unset = new_input[prop] === "{{unset}}";
       const is_empty_object =
         typeof current_v === "object" &&
         Object.keys(new_input[prop]).length === 0;
-      if (is_empty_object) {
+      if (new_v_is_unset) {
+        // delete
+        this.logger.info(`${prop} is deleted`);
+        delete source[prop];
+      } else if (is_empty_object) {
         // force to an empty object
         source[prop] = {};
       } else if (is_array) {
         // anything else we have to assume its a new value
         // this includes arrays since those inner values may be entirely new
+        this.logger.info(`${prop} is array`);
         source[prop] = new_input[prop];
       } else if (typeof current_v === "object") {
+        this.logger.info(`${prop} is an object and needs a merge`);
         // we need to merge these objects
         // since the two properties should be the same between the two overrides we can  assume
         // that it exist and is t he same type. if not, weird things will happen
@@ -154,6 +161,7 @@ class LC_Patch_Bosses implements IPreAkiLoadMod, IPostDBLoadMod {
           new_input[prop] as Record<any, any>
         );
       } else {
+        this.logger.info(`${prop} is a regular value can be set normally`);
         // anything else we have to assume its a new value
         // this includes arrays since those inner values may be entirely new
         source[prop] = new_input[prop];
