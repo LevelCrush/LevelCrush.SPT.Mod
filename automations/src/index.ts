b6934a20a9b7ee6bae322cf48ea2b21552df986a
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-05-07 16:20:41 -0400

    Automations skeleton

---
+++ automations/src/index.ts
@@ -0,0 +1,7 @@
+async function main() {
+    console.log('Starting automations');
+}
+
+main()
+    .then(() => console.log('Completed'))
+    .catch((err) => console.log('An Error Has Occurred', err));
