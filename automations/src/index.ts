ec068d74f23de8756b0241364cf8fcdb8ea04cd6
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-05-07 17:24:07 -0400

    Setup profile backup job

--- automations/src/index.ts
+++ automations/src/index.ts
@@ -1,5 +1,42 @@
+import dotenv from 'dotenv';
+import { ParseArgsConfig, parseArgs } from 'node:util';
+import * as backup from './jobs/backup';
+
+// allow environment variables to load from a .env
+dotenv.config();
+
 async function main() {
-    console.log('Starting automations');
+    console.log('Starting');
+    const options = {
+        job: {
+            type: 'string',
+            default: 'backup:profiles',
+        },
+    } as ParseArgsConfig['options'];
+
+    // destructure on the left, the result we get on the right
+    const { values, positionals } = parseArgs({ options, allowPositionals: true });
+
+    if (positionals.length === 0) {
+        console.log('No job specified. Please specify job');
+        process.exit();
+    }
+
+    let job = undefined;
+    switch (positionals[0].trim().toLowerCase()) {
+        case 'backup:profiles':
+            job = backup.profiles();
+            break;
+        default:
+            console.warn(`No known command ${positionals[0]}`);
+            break;
+    }
+
+    if (job !== undefined) {
+        console.log(`Running job ${job.name}`);
+        await job.setup();
+        await job.run();
+    }
 }
 
 main()
