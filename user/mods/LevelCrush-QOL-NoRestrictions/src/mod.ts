498f63fd1c0e306f2097155f3864fc4ea39f1d8f
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-05-09 07:23:52 -0400

    Prices adjusted for event. Patched LiveFleaPrices to account for this

--- user/mods/LevelCrush-QOL-NoRestrictions/src/mod.ts
+++ user/mods/LevelCrush-QOL-NoRestrictions/src/mod.ts
@@ -13,8 +13,9 @@ import { ITraderConfig } from '@spt-aki/models/spt/config/ITraderConfig';
 import { IRagfairConfig } from '@spt-aki/models/spt/config/IRagfairConfig';
 import { JsonUtil } from '@spt-aki/utils/JsonUtil';
 import { HashUtil } from '@spt-aki/utils/HashUtil';
+import { IDatabaseTables } from '@spt-aki/models/spt/server/IDatabaseTables';
 
-class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
+export class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
     private mod: string;
     private logger: ILogger;
 
@@ -32,12 +33,20 @@ class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
         this.logger.debug(`[${this.mod}] preAki Loading... `);
 
         // Get SPT code/data we need later
-        const preAkiModLoader: PreAkiModLoader = container.resolve<PreAkiModLoader>('PreAkiModLoader');
-        const imageRouter: ImageRouter = container.resolve<ImageRouter>('ImageRouter');
-        const hashUtil: HashUtil = container.resolve<HashUtil>('HashUtil');
         const configServer = container.resolve<ConfigServer>('ConfigServer');
-        const traderConfig: ITraderConfig = configServer.getConfig<ITraderConfig>(ConfigTypes.TRADER);
         const ragfairConfig = configServer.getConfig<IRagfairConfig>(ConfigTypes.RAGFAIR);
+
+        // disable bsg blacklist
+        this.logger.info('No restrictions is removing the bsg blacklist');
+        ragfairConfig.dynamic.blacklist.enableBsgList = false;
+
+        // allowing armor class 6 plates
+        // disable euros from being generated on the flea market
+        this.logger.info('Removing EUROS from being listable');
+        ragfairConfig.dynamic.currencies['569668774bdc2da2298b4568'] = 0;
+
+        this.logger.info('Max Armor Plates allowed are armor class 6');
+        ragfairConfig.dynamic.blacklist.armorPlate.maxProtectionLevel = 6;
 
         this.logger.debug(`[${this.mod}] preAki Loaded`);
     }
@@ -51,11 +60,13 @@ class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
 
         // Resolve SPT classes we'll use
         const databaseServer: DatabaseServer = container.resolve<DatabaseServer>('DatabaseServer');
-        const configServer: ConfigServer = container.resolve<ConfigServer>('ConfigServer');
-        const jsonUtil: JsonUtil = container.resolve<JsonUtil>('JsonUtil');
+        const configServer = container.resolve<ConfigServer>('ConfigServer');
+        const ragfairConfig = configServer.getConfig<IRagfairConfig>(ConfigTypes.RAGFAIR);
 
         // Get a reference to the database tables
         const tables = databaseServer.getTables();
+
+        const handbook_searches = [];
 
         if (tables.globals) {
             this.logger.debug('Lifting Restrictions on all items');
@@ -64,13 +75,43 @@ class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
                 restriction['MaxInRaid'] = 999999;
 
                 const template_id = restriction.TemplateId;
-                tables.templates.items[template_id]._props.DiscardLimit = 999999;
+                tables.templates.items[template_id]._props.DiscardLimit = -1;
+                tables.templates.items[template_id]._props.CanSellOnRagfair = true;
             }
-            this.logger.debug('Done Lifting Restrictions on all items');
+
+            ammo_flea_market(this.logger, tables);
         }
 
         this.logger.debug(`[${this.mod}] postDb Loaded`);
     }
 }
 
-module.exports = { mod: new LC_QOL_NoRestrictions() };
+export function ammo_flea_market(logger: ILogger, tables: IDatabaseTables) {
+    const handbook_searches = [];
+    logger.info('Scanning item database for ammunition');
+    for (const template_id in tables.templates.items) {
+        if (
+            tables.templates.items[template_id]._props.ammoType &&
+            tables.templates.items[template_id]._props.ammoType === 'bullet'
+        ) {
+            handbook_searches.push(template_id);
+        }
+    }
+
+    logger.info(`Adjusting pricing for ammo on ${handbook_searches.length} items`);
+    for (let i = 0; i < tables.templates.handbook.Items.length; i++) {
+        if (handbook_searches.includes(tables.templates.handbook.Items[i].Id)) {
+            const template_id = tables.templates.handbook.Items[i].Id;
+            logger.info(
+                `Adjusting base flea market price for ${tables.templates.items[template_id]._name} to be 6x normal rate`,
+            );
+            tables.templates.prices[template_id] = tables.templates.handbook.Items[i].Price * 6;
+        }
+    }
+    logger.debug('Done Lifting Restrictions on all items');
+}
+
+//module.exports = { mod: new LC_QOL_NoRestrictions() };
+
+export const mod = new LC_QOL_NoRestrictions();
+export default LC_QOL_NoRestrictions;
