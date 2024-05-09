5f7966c544114c1e172a477b4532258e51421b13
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-05-09 06:22:00 -0400

    Discard limit restrictions on all items lifted to...alot

--- user/mods/LevelCrush-QOL-NoRestrictions/src/mod.ts
+++ user/mods/LevelCrush-QOL-NoRestrictions/src/mod.ts
@@ -1,81 +1,76 @@
-import { DependencyContainer } from "tsyringe";
+import { DependencyContainer } from 'tsyringe';
 
 // SPT types
-import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
-import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
-import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-import { PreAkiModLoader } from "@spt-aki/loaders/PreAkiModLoader";
-import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
-import { ImageRouter } from "@spt-aki/routers/ImageRouter";
-import { ConfigServer } from "@spt-aki/servers/ConfigServer";
-import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
-import { ITraderConfig } from "@spt-aki/models/spt/config/ITraderConfig";
-import { IRagfairConfig } from "@spt-aki/models/spt/config/IRagfairConfig";
-import { JsonUtil } from "@spt-aki/utils/JsonUtil";
-import { HashUtil } from "@spt-aki/utils/HashUtil";
+import { IPreAkiLoadMod } from '@spt-aki/models/external/IPreAkiLoadMod';
+import { IPostDBLoadMod } from '@spt-aki/models/external/IPostDBLoadMod';
+import { ILogger } from '@spt-aki/models/spt/utils/ILogger';
+import { PreAkiModLoader } from '@spt-aki/loaders/PreAkiModLoader';
+import { DatabaseServer } from '@spt-aki/servers/DatabaseServer';
+import { ImageRouter } from '@spt-aki/routers/ImageRouter';
+import { ConfigServer } from '@spt-aki/servers/ConfigServer';
+import { ConfigTypes } from '@spt-aki/models/enums/ConfigTypes';
+import { ITraderConfig } from '@spt-aki/models/spt/config/ITraderConfig';
+import { IRagfairConfig } from '@spt-aki/models/spt/config/IRagfairConfig';
+import { JsonUtil } from '@spt-aki/utils/JsonUtil';
+import { HashUtil } from '@spt-aki/utils/HashUtil';
 
 class LC_QOL_NoRestrictions implements IPreAkiLoadMod, IPostDBLoadMod {
-  private mod: string;
-  private logger: ILogger;
+    private mod: string;
+    private logger: ILogger;
 
-  constructor() {
-    this.mod = "LevelCrush-QOL-NoRestrictions"; // Set name of mod so we can log it to console later
-  }
-
-  /**
-   * Some work needs to be done prior to SPT code being loaded, registering the profile image + setting trader update time inside the trader config json
-   * @param container Dependency container
-   */
-  public preAkiLoad(container: DependencyContainer): void {
-    // Get a logger
-    this.logger = container.resolve<ILogger>("WinstonLogger");
-    this.logger.debug(`[${this.mod}] preAki Loading... `);
-
-    // Get SPT code/data we need later
-    const preAkiModLoader: PreAkiModLoader =
-      container.resolve<PreAkiModLoader>("PreAkiModLoader");
-    const imageRouter: ImageRouter =
-      container.resolve<ImageRouter>("ImageRouter");
-    const hashUtil: HashUtil = container.resolve<HashUtil>("HashUtil");
-    const configServer = container.resolve<ConfigServer>("ConfigServer");
-    const traderConfig: ITraderConfig = configServer.getConfig<ITraderConfig>(
-      ConfigTypes.TRADER
-    );
-    const ragfairConfig = configServer.getConfig<IRagfairConfig>(
-      ConfigTypes.RAGFAIR
-    );
-
-    this.logger.debug(`[${this.mod}] preAki Loaded`);
-  }
-
-  /**
-   * Majority of trader-related work occurs after the aki database has been loaded but prior to SPT code being run
-   * @param container Dependency container
-   */
-  public postDBLoad(container: DependencyContainer): void {
-    this.logger.debug(`[${this.mod}] postDb Loading... `);
-
-    // Resolve SPT classes we'll use
-    const databaseServer: DatabaseServer =
-      container.resolve<DatabaseServer>("DatabaseServer");
-    const configServer: ConfigServer =
-      container.resolve<ConfigServer>("ConfigServer");
-    const jsonUtil: JsonUtil = container.resolve<JsonUtil>("JsonUtil");
-
-    // Get a reference to the database tables
-    const tables = databaseServer.getTables();
-
-    if (tables.globals) {
-      this.logger.debug("Lifting Restrictions on all items");
-      for (const restriction of tables.globals.config.RestrictionsInRaid) {
-        restriction["MaxInLobby"] = 999999;
-        restriction["MaxInRaid"] = 999999;
-      }
-      this.logger.debug("Done Lifting Restrictions on all items");
+    constructor() {
+        this.mod = 'LevelCrush-QOL-NoRestrictions'; // Set name of mod so we can log it to console later
     }
 
-    this.logger.debug(`[${this.mod}] postDb Loaded`);
-  }
+    /**
+     * Some work needs to be done prior to SPT code being loaded, registering the profile image + setting trader update time inside the trader config json
+     * @param container Dependency container
+     */
+    public preAkiLoad(container: DependencyContainer): void {
+        // Get a logger
+        this.logger = container.resolve<ILogger>('WinstonLogger');
+        this.logger.debug(`[${this.mod}] preAki Loading... `);
+
+        // Get SPT code/data we need later
+        const preAkiModLoader: PreAkiModLoader = container.resolve<PreAkiModLoader>('PreAkiModLoader');
+        const imageRouter: ImageRouter = container.resolve<ImageRouter>('ImageRouter');
+        const hashUtil: HashUtil = container.resolve<HashUtil>('HashUtil');
+        const configServer = container.resolve<ConfigServer>('ConfigServer');
+        const traderConfig: ITraderConfig = configServer.getConfig<ITraderConfig>(ConfigTypes.TRADER);
+        const ragfairConfig = configServer.getConfig<IRagfairConfig>(ConfigTypes.RAGFAIR);
+
+        this.logger.debug(`[${this.mod}] preAki Loaded`);
+    }
+
+    /**
+     * Majority of trader-related work occurs after the aki database has been loaded but prior to SPT code being run
+     * @param container Dependency container
+     */
+    public postDBLoad(container: DependencyContainer): void {
+        this.logger.debug(`[${this.mod}] postDb Loading... `);
+
+        // Resolve SPT classes we'll use
+        const databaseServer: DatabaseServer = container.resolve<DatabaseServer>('DatabaseServer');
+        const configServer: ConfigServer = container.resolve<ConfigServer>('ConfigServer');
+        const jsonUtil: JsonUtil = container.resolve<JsonUtil>('JsonUtil');
+
+        // Get a reference to the database tables
+        const tables = databaseServer.getTables();
+
+        if (tables.globals) {
+            this.logger.debug('Lifting Restrictions on all items');
+            for (const restriction of tables.globals.config.RestrictionsInRaid) {
+                restriction['MaxInLobby'] = 999999;
+                restriction['MaxInRaid'] = 999999;
+
+                const template_id = restriction.TemplateId;
+                tables.templates.items[template_id]._props.DiscardLimit = 999999;
+            }
+            this.logger.debug('Done Lifting Restrictions on all items');
+        }
+
+        this.logger.debug(`[${this.mod}] postDb Loaded`);
+    }
 }
 
 module.exports = { mod: new LC_QOL_NoRestrictions() };
